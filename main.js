81e9566a7a8130a1c4d8cf83a648a6eddb876025
Julien Huang <julien.huang@outlook.fr>
2020-05-17 16:05:16 +0200

    feat(skills) : added animations

--- main.js
+++ main.js
@@ -8,10 +8,35 @@ var isInViewport = function (elem) {
 };
 
 //animations
-
-$('window').on("scroll", function () {
-	if (isInViewport(document.querySelector('.progressbar_inline')){
-		$('.progressbar_inline').addClass('animation_progress_90')
+//profil animations
+window.addEventListener('scroll', function () {
+	if (isInViewport(document.querySelector('.profil_container'))) {
+		for (let i=1; i <= 6; i++) {
+			$('.profil_block_' + i).addClass('animation_showUp_' + i)
+        }
 
 	}
 })
+
+//skills animations
+window.addEventListener('scroll',function() {
+	if (isInViewport(document.querySelector('.progressbar_inline'))) {
+		$('.progressbar_90').addClass('animation_progress_90')
+		$('.progressbar_95').addClass('animation_progress_95')
+		$('.progressbar_85').addClass('animation_progress_85')
+		$('.progressbar_70').addClass('animation_progress_70')
+		$('.progressbar_20').addClass('animation_progress_20')
+		$('.progressbar_25').addClass('animation_progress_25')
+	}
+	if (isInViewport(document.querySelector('.level'))) {
+		for (let i = 1; i <= 5; i++) {
+			setTimeout(function () { $('.level_master_' + i).addClass('level_on') }, 400*i)
+		}
+		setTimeout(function () { $('.level_beginner_1').addClass('level_on') },400)
+    }
+	if (isInViewport(document.querySelector('.progressbar_frameworks_55'))) {
+		$('.progressbar_frameworks_55').addClass('animation_progress_55')
+		$('.progressbar_frameworks_90').addClass('animation_progress_90')
+    }
+})
+
